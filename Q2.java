b6936f0c3d12f45cf6a2e13330ae726fd185ef3b
Sahil Garg <69622920+SahilGarg28@users.noreply.github.com>
2021-08-04 15:57:37 +0530

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q2 {
+public static void main(String[] args) {
+sum s=new sum();
+s.input();
+System.out.println(s.process());
+}
+}
+class sum{
+	Scanner cin=new Scanner(System.in);
+	float a,b;
+	void input()
+	{
+		System.out.print("Enter 1st number: ");
+		a=cin.nextFloat();
+		System.out.print("Enter 2nd number: ");
+		b=cin.nextFloat();
+	}
+	float process()
+	{	
+		return a+b;
+		
+	}
+	
+}
